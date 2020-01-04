a2d19aaa385c07059c9479b6f5bf32cb140a99a5
ibnbasri20 <epibn70@gmail.com>
2020-01-04 09:53:22 +0700

    added migration

--- database/migrations/2020_01_04_024014_user_info.php
+++ database/migrations/2020_01_04_024014_user_info.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class UsersInfo extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users_info', function(Blueprint $table){
             $table->uuid('id')->primary();
-            $table->string('email')->unique();
-            $table->string('useranme')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->timestamps();
+            $table->uuid('users_id')->index();
+            $table->string('first_name');
+            $table->sting('last_name');
+            $table->sting('photo')->nullable();
+            $table->uuid('group_id');
         });
     }
 
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('users_info');
     }
 }
---
+++ database/migrations/2020_01_04_024757_group_users.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class GroupUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_users', function(Blueprint $table)
+        {
+            $table->uuid('id');
+            $table->string('group_name');
+            $table->uuid('users_id')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExits('group_users');
+    }
+}
