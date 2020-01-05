9105edf1e0acf1453697624491490a4efd878692
ibnbasri20 <epibn70@gmail.com>
2020-01-05 09:52:08 +0700

    add user_information

---
+++ app/Http/Controllers/API/UserAPI.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+
+class UserAPI extends Controller
+{
+    public function show(Request $request, $id)
+    {
+
+    }
+
+    public function update_profile(Request $request, $id)
+    {
+
+    }
+
+    public function change_password(Request $request)
+    {
+
+    }
+
+    public function request_delete_user(Request $request , $id)
+    {
+       
+    }
+}
